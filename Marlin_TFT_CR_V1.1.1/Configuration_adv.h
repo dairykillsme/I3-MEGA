7630b708b5435cd89e624f965892b1b0981a0fad
George Philbrick <gphilbri@gmail.com>
2022-04-21 15:15:13 -0400

    Tuned thermal safeties for larger hot end

--- Marlin_TFT_CR_V1.1.1/Configuration_adv.h
+++ Marlin_TFT_CR_V1.1.1/Configuration_adv.h
@@ -74,7 +74,7 @@
  * If you get false positives for "Thermal Runaway" increase THERMAL_PROTECTION_HYSTERESIS and/or THERMAL_PROTECTION_PERIOD
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
-  #define THERMAL_PROTECTION_PERIOD 250        // Seconds-
+  #define THERMAL_PROTECTION_PERIOD 60        // Seconds-
   #define THERMAL_PROTECTION_HYSTERESIS 45     // Degrees Celsius
 
   /**
@@ -86,8 +86,8 @@
    * If you get false positives for "Heating failed" increase WATCH_TEMP_PERIOD and/or decrease WATCH_TEMP_INCREASE
    * WATCH_TEMP_INCREASE should not be below 2.
    */
-  #define WATCH_TEMP_PERIOD 35                // Seconds
-  #define WATCH_TEMP_INCREASE 10               // Degrees Celsius
+  #define WATCH_TEMP_PERIOD 60                // Seconds
+  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
 #endif
 
 /**
@@ -1101,7 +1101,7 @@
 /**
  * Auto-report temperatures with M155 S<seconds>
  */
-//#define AUTO_REPORT_TEMPERATURES
+#define AUTO_REPORT_TEMPERATURES
 
 /**
  * Include capabilities in M115 output
